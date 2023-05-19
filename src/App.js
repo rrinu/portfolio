7931f2b274b744efc4fb6778eed5ac261724058a
Rinu Rahim <89441597+rrinu@users.noreply.github.com>
2023-05-19 18:21:20 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
           <GlobalStyles />
           <Navbar />
           <Home />
-          <Projects />
           <About />
+          <Projects />
           <Technologies />
           <Footer />
         </>
